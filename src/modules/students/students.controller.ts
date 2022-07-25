bff1585718d456caf7f7b7a34b5d759fc32f47be
Armen96 <barsegyan96armen@gmail.com>
2022-07-26 02:03:10 +0400

    Fake controller endpoints

--- src/modules/students/students.controller.ts
+++ src/modules/students/students.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  // Req,
+  // Res,
+  ParseIntPipe, HttpException, HttpStatus,
+} from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+// import { Request, Response} from 'express';
 
 @Controller('students')
 export class StudentsController {
@@ -15,6 +28,23 @@ export class StudentsController {
   @Get()
   findAll() {
     return this.studentsService.findAll();
+  }
+
+  @Get('fake')
+  findFakeStudent() { // @Req() req: Request, @Res() res: Response
+    // Use express req and res -> res.send(401)
+    return {id: 'test', name: 'test'};
+  }
+
+  @Get('fake/:fakeId')
+  findFakeStudentWithParam(@Param('fakeId', ParseIntPipe) fakeId: number) { // @Req() req: Request, @Res() res: Response
+    // Use express req and res -> res.send(401)
+    return {id: 'test', name: 'test', fake_id: fakeId};
+  }
+
+  @Get('error')
+  errorHandler() {
+    return new HttpException('Customer not Found', HttpStatus.NOT_FOUND)
   }
 
   @Get(':id')
